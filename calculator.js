dd12de9362924494c58528a7a4e18a0fcd0174c6
xavipro <st22030273@utlaguna.edu.mx>
2025-11-10 12:05:21 -0600

    divide function added

--- calculator.js
+++ calculator.js
@@ -9,3 +9,10 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b;
 }
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return a / b;
+}
